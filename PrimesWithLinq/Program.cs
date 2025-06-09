28f52ebdc42d0da8537855a256b3c84286a4437b
fred <domaine75@gmail.com>
2025-06-09 13:54:28 +0200

    add saving result to a file

--- PrimesWithLinq/Program.cs
+++ PrimesWithLinq/Program.cs
@@ -12,7 +12,8 @@ namespace PrimesWithLinq
       sw.Start();
 
       int minimumNumber = 2;
-      int maximumNumber = 1000000; // retourne 78 498 en ~1 ms
+      int maximumNumber = 1_000_000; // retourne 78 498 en ~1 ms
+      //int maximumNumber = int.MaxValue; // int.MaxValue = 2 147 483 647
 
       var primes = from n in Enumerable.Range(minimumNumber, maximumNumber - minimumNumber).AsParallel()
                    where Enumerable.Range(1, (int)Math.Sqrt(n)).All(v => v == 1 || n % v != 0)
@@ -23,7 +24,9 @@ namespace PrimesWithLinq
       Console.WriteLine();
       Console.WriteLine($"De {minimumNumber} à {maximumNumber}, {primes.Count()} nombres premiers trouvés en {sw.ElapsedMilliseconds} ms.");
       Console.WriteLine(string.Empty);
-      foreach (var prime in primes.OrderBy(i => i).Take(500)) // affiche les 100 premiers nombres premiers trouvés
+      // affiche les 500 premiers nombres premiers trouvés
+      SavePrimesToFile(primes, $"Primes-{primes.Count()}.txt");
+      foreach (var prime in primes.OrderBy(i => i).Take(500))
       {
         Console.Write($"{prime} ");
       }
@@ -31,6 +34,18 @@ namespace PrimesWithLinq
       Console.WriteLine(string.Empty);
       Console.WriteLine("Appuyez sur une touche pour quitter.");
       Console.ReadKey();
+    }
+
+    private static void SavePrimesToFile(ParallelQuery<int> primes, string filename)
+    {
+      // Sauvegarde les nombres premiers dans un fichier texte avec un nombre par ligne
+      using (var writer = new System.IO.StreamWriter(filename))
+      {
+        foreach (var prime in primes.OrderBy(i => i))
+        {
+          writer.WriteLine(prime);
+        }
+      }
     }
   }
 }
